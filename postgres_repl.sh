e497a9d413b7399d8e73ddf8da4698d758129c02
Christopher Baker <cgbaker@gmail.com>
2016-12-12 17:12:02 -0500

    finally have a version that is working with re-entrant containers and promotion added promote script needs more testing

--- postgres_repl.sh
+++ postgres_repl.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-#set -o xtrace
+set -o xtrace
 
 PGREPL_ROLE=${PGREPL_ROLE-(none)}
 echo "****** PGREPL_ROLE is: $PGREPL_ROLE"
@@ -11,25 +11,21 @@ echo "****** PGREPL_ROLE is: $PGREPL_ROLE"
 PGREPL_TOKEN=${PGREPL_TOKEN-abc123}
 PGREPL_TOKEN_HASH=$(echo $PGREPL_TOKEN | md5sum | awk '{print $1}')
 
-# sentinel files to facilitate re-entrant containers
-sentinel_s="$PGDATA"/pgrepl_standby
-sentinel_p="$PGDATA"/pgrepl_primary
 trigger_file="$PGDATA"/pgrepl_trigger
 recovery_file="$PGDATA"/recovery.conf
+sentinel_file="$PGDATA"/pgrepl_sentinel.conf
 
-if [ -e ${sentinel_s} ]; then 
-  PREV_STANDBY=1
-else
-  PREV_STANDBY=0
-fi
-if [ -e ${sentinel_p} ]; then 
-  PREV_PRIMARY=1
-else
-  PREV_PRIMARY=0
-fi
-
-echo "****** Clearing pre-existing sentinel files"
-rm -f ${sentinel_s} ${sentinel_p}
+initialize_replication() {
+  echo "****** Setting postgres system variables for replication"
+  gosu postgres postgres --single <<- EOSQL
+    ALTER SYSTEM SET wal_level=hot_standby;
+    ALTER SYSTEM SET max_wal_senders=10; 
+    ALTER SYSTEM SET hot_standby=on;
+    CREATE USER pgrepl WITH replication password '${PGREPL_TOKEN_HASH}';
+EOSQL
+  
+  echo "host  replication     pgrepl     0.0.0.0/0         md5" >> "$PGDATA"/pg_hba.conf
+}
 
 setup_standby() {
   if [ -z "$PGREPL_MASTER_IP" ]; then
@@ -54,52 +50,27 @@ setup_standby() {
   gosu postgres echo "primary_conninfo='host=$PGREPL_MASTER_IP port=$PGREPL_MASTER_PORT user=pgrepl'" >> ${recovery_file}
   gosu postgres echo "recovery_target_timeline = 'latest'"                                            >> ${recovery_file}
   gosu postgres echo "trigger_file = '$trigger_file'"                                                 >> ${recovery_file}
-
-  echo "****** Creating standby sentinel: $sentinel_s"
-  touch "$sentinel_s"
 }
 
 setup_primary() {
-  if [ $PREV_STANDBY -ne 0 ]; then 
-    echo "****** Detected promotion; will create trigger file to initiate promotion"
+  if [ -e ${sentinel_file} ]; then
+    echo "****** Sentinel file detected; will create trigger file to trigger promotion."
     touch ${trigger_file}
-  else
-    echo "****** Did not detect promotion."
-  fi 
-  echo "****** Creating primary sentinel: $sentinel_p"
-  touch "$sentinel_p"
+  fi
 }
 
-initialize_replication() {
-  echo "****** Setting postgres system variables for replication"
-  gosu postgres postgres --single <<- EOSQL
-    ALTER SYSTEM SET wal_level=hot_standby;
-    ALTER SYSTEM SET max_wal_senders=10; 
-    ALTER SYSTEM SET hot_standby=on;
-    CREATE USER pgrepl WITH replication password '${PGREPL_TOKEN_HASH}';
-EOSQL
-
-  echo "host  replication     pgrepl     0.0.0.0/0         md5" >> "$PGDATA"/pg_hba.conf
-}
-
-if (( $PREV_PRIMARY || $PREV_STANDBY )); then 
-    echo "****** Sentinels detected:"
-    echo "****** PRIMARY: $PREV_PRIMARY"
-    echo "****** STANDBY: $PREV_STANDBY"
-    echo "****** Will not initialize replication."
-else 
-    echo "****** No sentinels detected. Will initialize replication."
-    initialize_replication
+if [ ! -e ${sentinel_file} ]; then 
+  initialize_replication
 fi
 
 case "$PGREPL_ROLE" in 
-  "PRIMARY") 
-    setup_primary
-    ;;
   "STANDBY") 
     setup_standby
     ;;
   *) 
-    echo "****** UNRECOGNIZED PGREPL_ROLE: $PGREPL_ROLE"
+    setup_primary
     ;;
 esac
+
+echo "****** Creating sentinel file"
+touch ${sentinel_file}
